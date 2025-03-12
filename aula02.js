77d296ba90eacba249f54356b681b7e1bb87516e
Gaby Ramos <74305504+BihRamos@users.noreply.github.com>
2025-03-12 20:14:38 -0300

    Add files via upload

---
+++ aula02.js
@@ -0,0 +1,14 @@
+
+"user strict"
+nome = "Gaby"//variavel nome com modo restrito //
+console.log(nome);//variavel//
+console.log("Gaby");//string//
+console.log("Nome:"+nome);//string com variavel//
+
+
+/*
+nome = "Gaby"//variavel nome sem modo restrito //
+console.log(nome);//variavel//
+console.log("Gaby");//string//
+console.log("Nome:"+nome);//string com variavel//
+*/
