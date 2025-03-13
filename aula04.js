8f727bcca90e7f590b3c2ae44cc8ee782467db54
Gaby Ramos <74305504+BihRamos@users.noreply.github.com>
2025-03-13 10:36:04 -0300

    Add files via upload

---
+++ aula04.js
@@ -0,0 +1,24 @@
+/*
+let num1=0,num2=0,res=0;
+
+num1=2;
+num2=15;
+
+res=num2%num1
+
+console.log(num2/num1)//divisão//
+console.log(num2%num1)//resto//
+*/
+
+let num1=0,num2=0,res=0;
+
+num1=30;
+num2=15;
+
+num1++ //operador de  incremento//
+num1--//operador de decremento//
+num1+=1 //semelhante ao num1++ (num1=num1+1)
+num1+=5 //num1=num1+5
+num1*=2 //multiplica o valor da variavel por 2//
+num1/=2 // divide o valor da variavel por 2//
+console.log(num1)
