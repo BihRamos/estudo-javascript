32460829d76f9040c0e4e5a768a0319f9e149a6f
Gaby Ramos <74305504+BihRamos@users.noreply.github.com>
2025-03-16 13:08:12 -0300

    Add files via upload

---
+++ aula07.js
@@ -0,0 +1,23 @@
+// && -> and e 
+// || -> or ou
+// !  -> not não 
+
+let n1,n2,n3,n4;
+n1=10;
+n2=5;
+n3=15;
+n4=2;
+
+/*console.log((n1>n2)&&(n1>n3));
+console.log((n1>n2)||(n1>n3));
+console.log(!((n1>n2)||(n1>n3)));*/
+
+if(!((n3>n2) || (n1>n4))){
+   console.log("verdadeiro")
+}else{
+   console.log("falso")
+}
+
+
+
+   
