921e73e8cf3f1cbb51e3d81fd34c9705f60ce5e0
Gaby Ramos <74305504+BihRamos@users.noreply.github.com>
2025-03-19 21:51:07 -0300

    Add files via upload

---
+++ aula09.js
@@ -0,0 +1,25 @@
+/*Pré-incremento (++let)
+✔️ Incrementa antes de usar o valor.
+✔️ Retorna o valor já incrementado.
+
+ Pós-incremento (let++)
+✔️ Usa o valor primeiro, depois incrementa.
+✔️ Retorna o valor antes da adição.
+
+Quando usar cada um?
+Use ++let (pré-incremento) se precisar do valor já incrementado imediatamente.
+Use let++ (pós-incremento) se precisar do valor original antes de incrementar.
+
+*/
+
+let n=10;
+
+n++  //pós incremento
+
+console.log(++n) //pré incremento
+
+console.log(n)
+
+console.log(n++)
+
+console.log(n)
