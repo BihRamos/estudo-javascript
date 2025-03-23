dec21dc66b841eb6cf5cc7fdaf3acdc9f469666c
Gaby Ramos <74305504+BihRamos@users.noreply.github.com>
2025-03-22 21:29:42 -0300

    Add files via upload

---
+++ aula18.js
@@ -0,0 +1,11 @@
+/*O do while é um loop de repetição que executa o código pelo menos uma vez, e depois continua repetindo enquanto a condição for 
+verdadeira.*/
+
+let n=10
+
+do{
+    console.log("Bih Ramos")
+    n++
+}while(n<10)
+
+console.log("Fim do programa")
